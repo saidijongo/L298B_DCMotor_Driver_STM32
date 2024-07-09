91cc769753e648740fc3cb0507cfd98b935f893f
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2024-07-09 11:38:24 +0900

    Create pin_map.h

---
+++ pin_map.h
@@ -0,0 +1,30 @@
+/*
+ * pin_map.h
+ *
+ *  Created on: Jul 9, 2024
+ *      Author: 999ga
+ */
+
+#ifndef PIN_MAP_H
+#define PIN_MAP_H
+
+// Motor control pins
+#define IN1_Pin GPIO_PIN_8
+#define IN1_GPIO_Port GPIOA
+
+#define IN2_Pin GPIO_PIN_9
+#define IN2_GPIO_Port GPIOA
+
+#define EN_Pin GPIO_PIN_10
+#define EN_GPIO_Port GPIOA
+
+// Button pin
+#define BUTTON_Pin GPIO_PIN_13
+#define BUTTON_GPIO_Port GPIOC
+
+// Internal LED pin
+#define LED_Pin GPIO_PIN_5
+#define LED_GPIO_Port GPIOA
+
+#endif /* PIN_MAP_H */
+
